eb4390ae9cb72e411e965b2847add47beda9a8f3
zekizafer <zekizafer.aydinli@hotmail.com>
2017-07-04 14:42:19 +0200

    added new variables

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
     private String multiplicate(int a, int b){
         int result = a*b;
+        int random = 4534626;
+        String name = "Zeki Zafer";
         return "Why you so mad?";
     }
 }
