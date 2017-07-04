7c9fff14f8bae78c310fb04e1648278136f3f784
zekizafer <zekizafer.aydinli@hotmail.com>
2017-07-04 14:34:17 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World! " + new Main().multiplicate(5,6));
+    }
+
+    private String multiplicate(int a, int b){
+        int result = a*b;
+        return "Why you so mad?";
+    }
+}
